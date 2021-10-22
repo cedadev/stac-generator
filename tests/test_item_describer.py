672773d9694c4d6f8e6d7247d3e87a86c8041bb6
rsmith013 <richard.d.smith@stfc.ac.uk>
2021-10-22 16:18:20 +0100

    adding tests to check can match posix paths and remote URIs

---
+++ tests/test_item_describer.py
@@ -0,0 +1,37 @@
+# encoding: utf-8
+"""
+
+"""
+__author__ = 'Richard Smith'
+__date__ = '22 Oct 2021'
+__copyright__ = 'Copyright 2018 United Kingdom Research and Innovation'
+__license__ = 'BSD - see LICENSE file in top-level package directory'
+__contact__ = 'richard.d.smith@stfc.ac.uk'
+
+import os
+
+import pytest
+
+from asset_scanner.core.item_describer import ItemDescriptions
+
+ROOT_DESCRIPTIONS = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_descriptions')
+
+
+@pytest.fixture
+def item_descriptions():
+    print(ROOT_DESCRIPTIONS)
+    return ItemDescriptions(ROOT_DESCRIPTIONS)
+
+
+def test_retrieve_posix_description(item_descriptions):
+    expected = {'datasets': ['/a/b/c']}
+
+    description = item_descriptions.get_description('/a/b/c/d/e')
+    assert description._description == expected
+
+
+def test_retrieve_remote_description(item_descriptions):
+    expected = {'datasets': ['gc://a/b/c']}
+
+    description = item_descriptions.get_description('gc://a/b/c/d/e')
+    assert description._description == expected
